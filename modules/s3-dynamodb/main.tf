ab5f110324b7b4ea97e2c306d6804caecf2f0b8d
jidebola <jidedomaining@gmail.com>
2024-10-29 12:38:44 +0000

    deprecated s3 codes rectified

--- modules/s3-dynamodb/main.tf
+++ modules/s3-dynamodb/main.tf
@@ -1,21 +1,30 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket
+}
 
-  versioning {
-    enabled = true
+ resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = var.bucket
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = var.bucket
+
+  rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
-    }
-  }
+}
+}
 
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = var.bucket
+
+  rule {
     id      = "log"
-    enabled = true
+    status = "Enabled"
 
     transition {
       days          = 30
@@ -25,7 +34,7 @@ resource "aws_s3_bucket" "terraform_state" {
     expiration {
       days = 365
     }
-  }
+}
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
@@ -38,3 +47,31 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+
+  # versioning {
+  #   enabled = true
+  # }
+
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+
+  # lifecycle_rule {
+  #   id      = "log"
+  #   enabled = true
+
+  #   transition {
+  #     days          = 30
+  #     storage_class = "GLACIER"
+  # #   }
+
+  #   expiration {
+  #     days = 365
+  #   }
+  # }
+
